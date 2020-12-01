1ec99c9c3715de869ab2b39185450c5ee8720af1
Emerentius <emerentius@arcor.de>
2020-12-01 15:19:05 +0100

    day 1: part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::collections::HashSet;
+
+enum Part {
+    One,
+    Two,
+}
+
+fn day1(part: Part) {
+    let input = include_str!("day1_input.txt");
+    let nums = input
+        .lines()
+        .map(|line| line.parse::<u64>().unwrap())
+        .collect::<HashSet<_>>();
+
+    let target = 2020;
+
+    let (num1, num2) = nums
+        .iter()
+        .find_map(|&num| {
+            let matching_num = nums.get(&(target - num))?;
+            Some((num, matching_num))
+        })
+        .unwrap();
+    println!("{} * {} = {}", num1, num2, num1 * num2);
+}
+
 fn main() {
-    println!("Hello, world!");
+    day1(Part::One);
 }
