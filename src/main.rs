9dedb1c6448b55b2db65d3f58f2a8bdd2adddb0e
Emerentius <emerentius@arcor.de>
2020-12-02 11:24:10 +0100

    day 2: part 1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 enum Part {
     One,
@@ -49,10 +49,46 @@ fn day1(part: Part) {
     );
 }
 
+fn day2(part: Part) {
+    let input = include_str!("day2_input.txt");
+    let pattern = regex::Regex::new(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)").unwrap();
+
+    let passwords = input
+        .lines()
+        .map(|line| {
+            let m = pattern.captures(line).unwrap();
+            let get = |i| m.get(i).unwrap().as_str();
+            let min = get(1).parse::<u64>().unwrap();
+            let max = get(2).parse::<u64>().unwrap();
+            let constrained_letter = get(3);
+            assert!(constrained_letter.len() == 1);
+            let constrained_letter = constrained_letter.chars().next().unwrap();
+            let password = get(4);
+
+            (min..=max, constrained_letter, password)
+        })
+        .collect::<Vec<_>>();
+
+    let n_valid_passwords = passwords
+        .iter()
+        .filter(|(letter_count_range, constrained_letter, password)| {
+            let mut letter_counts = HashMap::new();
+            for ch in password.chars() {
+                *letter_counts.entry(ch).or_insert(0) += 1;
+            }
+            let letter_count = letter_counts.get(&constrained_letter).copied().unwrap_or(0);
+            letter_count_range.contains(&letter_count)
+        })
+        .count();
+
+    println!("{}", n_valid_passwords);
+}
+
 fn main() {
     // keep solutions for old days here to avoid unused code warnings
     if false {
         day1(Part::One);
+        day1(Part::Two);
     }
-    day1(Part::Two);
+    day2(Part::One);
 }
